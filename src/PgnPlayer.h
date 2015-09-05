b05dc4bd194c004534a1ccf54c58832d69674783
unknown <gunnar.harms@chello.nl>
2015-09-05 09:58:33 +0200

    Changes

--- src/PgnPlayer.h
+++ src/PgnPlayer.h
@@ -7,7 +7,7 @@ class PGNPlayer : public PGNFileReader
 {
 public:
 	PGNPlayer(const char* path, bool check_legal = true) : PGNFileReader(path),
-		game_(nullptr), check_legal_(check_legal)
+		game_(nullptr)
 	{
 		game_ = new Game();
 	}
@@ -35,102 +35,100 @@ public:
 	virtual void readSANMove() {
 		PGNFileReader::readSANMove();
 
-		if (check_legal_) {
-			int piece = (side_to_move << 3);
+		int piece = (side_to_move << 3);
 
-			if (pawn_move_) {
-				piece |= Pawn;
-				game_->pos->generatePawnMoves(capture_, bbSquare(to_square));
+		if (pawn_move_) {
+			piece |= Pawn;
+			game_->pos->generatePawnMoves(capture_, bbSquare(to_square));
+		}
+		else if (castle_move_) {
+			piece |= King;
+			game_->pos->generateMoves();
+		}
+		else if (piece_move_) {
+				switch (from_piece) {
+				case 'N':
+					piece |= Knight;
+					break;
+				case 'B':
+					piece |= Bishop;
+					break;
+				case 'R':
+					piece |= Rook;
+					break;
+				case 'Q':
+					piece |= Queen;
+					break;
+				case 'K':
+					piece |= King;
+					break;
+				default:
+					cout<<"default ["<<token_str << "]"<<endl;
+					exit(0);
 			}
-			else if (castle_move_) {
-				piece |= King;
-				game_->pos->generateMoves();
-			}
-			else if (piece_move_) {
-					switch (from_piece) {
-					case 'N':
-						piece |= Knight;
-						break;
-					case 'B':
-						piece |= Bishop;
-						break;
-					case 'R':
-						piece |= Rook;
-						break;
-					case 'Q':
-						piece |= Queen;
-						break;
-					case 'K':
-						piece |= King;
-						break;
-					default:
-						cout<<"default ["<<token_str << "]"<<endl;
-						exit(0);
-				}
-				game_->pos->generateMoves(piece, bbSquare(to_square));
-			}
-			else {
-				cout<<"else"<<endl;
-				exit(0);
-			}
-			Piece promoted = (side_to_move << 3);
+			game_->pos->generateMoves(piece, bbSquare(to_square));
+		}
+		else {
+			cout<<"else"<<endl;
+			exit(0);
+		}
+		Piece promoted = (side_to_move << 3);
 
-			if (promoted_to != -1) {
-					switch (promoted_to) {
-					case 'N':
-						promoted |= Knight;
-						break;
-					case 'B':
-						promoted |= Bishop;
-						break;
-					case 'R':
-						promoted |= Rook;
-						break;
-					case 'Q':
-						promoted |= Queen;
-						break;
-					default:
-						cout<<"promoted_to error ["<<token_str << "]"<<endl;
-						exit(0);
-				}
+		if (promoted_to != -1) {
+				switch (promoted_to) {
+				case 'N':
+					promoted |= Knight;
+					break;
+				case 'B':
+					promoted |= Bishop;
+					break;
+				case 'R':
+					promoted |= Rook;
+					break;
+				case 'Q':
+					promoted |= Queen;
+					break;
+				default:
+					cout<<"promoted_to error ["<<token_str << "]"<<endl;
+					exit(0);
 			}
-			bool found = false;
-			int move_count = game_->pos->moveCount();
+		}
+		bool found = false;
+		int move_count = game_->pos->moveCount();
 
-			for (int i = 0; i < move_count; ++i) {
-				Move m = game_->pos->move_list[i].move;
+		for (int i = 0; i < move_count; ++i) {
+			Move m = game_->pos->move_list[i].move;
 
-				if ((movePiece(m) != piece)
-					|| ((int)moveTo(m) != to_square)
-					|| (promoted_to != -1 && movePromoted(m) != promoted)
-					|| (capture_ && !isCapture(m))
-					|| (from_file != -1 && ::file(moveFrom(m)) != from_file)
-					|| (from_rank != -1 && ::rank(moveFrom(m)) != from_rank))
-				{
+			if ((movePiece(m) != piece)
+				|| ((int)moveTo(m) != to_square)
+				|| (promoted_to != -1 && movePromoted(m) != promoted)
+				|| (capture_ && !isCapture(m))
+				|| (from_file != -1 && ::file(moveFrom(m)) != from_file)
+				|| (from_rank != -1 && ::rank(moveFrom(m)) != from_rank))
+			{
+				continue;
+			}
+
+			if (!game_->makeMove(m, true, true)) {
 					continue;
-				}
-
-				if (!game_->makeMove(m, true, true)) {
-						continue;
-				}
-				found = true;
-				break;
 			}
+			found = true;
+			break;
+		}
 
-			if (!found) {
-				cout<<"!found ["<<token_str << "]"<<endl;
-				cout << "to_square:"<<to_square<<endl;
-				cout<<"piece:"<<piece<<endl;
-				cout<<"from_file:"<<from_file<<endl;
-				cout<<"from_rank:"<<from_rank<<endl;
-				cout<<"pawn_move_:"<<pawn_move_<<endl;
-				cout<<"castle_move_:"<<castle_move_<<endl;
-				cout<<"side_to_move:"<<side_to_move<<endl;
-				cout<<"pos->in_check:"<<game_->pos->in_check<<endl;
-				cout<<"game_count_:"<<game_count_<<endl;
-				game_->board.print();
-				exit(0);
-			}
+		if (!found) {
+			cout<<"!found ["<<token_str << "]"<<endl;
+			cout << "to_square:"<<to_square<<endl;
+			cout<<"piece:"<<piece<<endl;
+			cout<<"from_file:"<<from_file<<endl;
+			cout<<"from_rank:"<<from_rank<<endl;
+			cout<<"pawn_move_:"<<pawn_move_<<endl;
+			cout<<"castle_move_:"<<castle_move_<<endl;
+			cout<<"side_to_move:"<<side_to_move<<endl;
+			cout<<"pos->in_check:"<<game_->pos->in_check<<endl;
+			cout<<"game_count_:"<<game_count_<<endl;
+			game_->board.print();
+			exit(0);
 		}
 	}
 
@@ -139,7 +137,6 @@ protected:
 
 private:
 	Move move_;
-	bool check_legal_;
 };
 
 } // namespace pgn
